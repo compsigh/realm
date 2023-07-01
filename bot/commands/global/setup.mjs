93c1020ef0e7d09a8f7bdfecd29b8d48cb2d74dc
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2023-07-01 15:00:24 -0700

    Auto-refresh guild commands on setup

--- bot/commands/global/setup.mjs
+++ bot/commands/global/setup.mjs
@@ -1,5 +1,6 @@
 import { SlashCommandBuilder } from '@discordjs/builders'
 import * as Discord from 'discord.js'
+import { commands } from '../../commands.mjs'
 import connect from '../../../functions/db-connect.mjs'
 import Server from '../../../schemas/server-schema.mjs'
 
@@ -18,21 +19,19 @@ const setupCommand = {
 
     // Create new server in database
     const newServer = new Server({
-      guildId: interaction.guildId
+      guildId: interaction.guildId,
+      lastRefresh: Date.now()
     })
     await newServer.save()
 
-    // Reply to user
-    await interaction.reply({ content: 'Realm has been set up in this server!', ephemeral: true })
-    // const helpEmbed = new Discord.EmbedBuilder()
-    //   .setColor('#75FB92')
-    //   .setTitle('Recap')
-    //   .setDescription('Summarize Discord messages, powered by AI')
-    //   .setFields([
-    //     { name: '`/tab`', value: 'Creates a temporary role with the people mentioned. Can be deleted when all debts are marked as paid.\n`/tab help` for more info', inline: false },
-    //     { name: '`/roadmap`', value: 'View planned features for Receipt', inline: false }
-    //   ])
-    // await interaction.reply({ embeds: [helpEmbed], ephemeral: true })
+    // Fetch commands the server has access to
+    await refresh(interaction.guildId, commands)
+
+    const setupSuccessEmbed = new Discord.EmbedBuilder()
+      .setColor('#FFFFFF')
+      .setTitle('Realm setup complete')
+      .setDescription('Realm has been successfully set up! The commands you have access to have been added to the server.')
+    await interaction.reply({ embeds: [setupSuccessEmbed], ephemeral: true })
   }
 }
 
